21e831c9f0a49250146254aafa861b3e01f587ea
mikedemis1 <mikedemis31@gmail.com>
2026-09-05 17:47:19 +0300

    style: terraform fmt on providers.tf, vpc.tf, subnets.tf
    No functional change.

--- subnets.tf
+++ subnets.tf
@@ -1,16 +1,16 @@
 resource "aws_subnet" "public" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "eu-west-1a"
-    tags = {
-    Name = "public-subnet"  }
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-1a"
+  tags = {
+  Name = "public-subnet" }
 
-  
+
 }
 
-resource "aws_subnet" "private"{
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-west-1a"
   tags = {
     Name = "private-subnet"
